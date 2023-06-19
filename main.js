ab36fb793ecb34af434f16234d723ab639011837
Miguel Cardoso <miguelcardoso1204@gmail.com>
2023-06-19 10:31:19 +0100

    add reset.css, finish styling, determine js steps

---
+++ main.js
@@ -0,0 +1,15 @@
+//attach event (submit) listener to the form to get user data
+
+//attach event (click) listeners to each square
+
+//initialize the game
+
+// check which gamemode we are on
+
+// set win conditions
+
+// determine current player
+
+// after each mpve, check if win conditions have been met, if not, set other player as active
+
+// human vs human, next implement easy ai, next impossible ai
